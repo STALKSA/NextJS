b79120502470933477e43fc5e5d95fb8f854b0a4
ksenia <ksy184694@gmail.com>
2023-09-02 12:02:26 +0300

    changed app file

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,15 @@
 import Header from "../components/Header";
-import Footer from "../components/footer";
-import '../styles/global.css';
+import Footer from "../components/Footer";
+
+import "../styles/global.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
-     <main><Component {...pageProps} /></main> 
+      <main>
+        <Component {...pageProps} />
+      </main>
       <Footer />
     </>
   );
